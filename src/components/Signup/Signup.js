502f8158a81e2f5b220e34e42d05938b893f40b0
vinay7816 <vinayreddy24729@gmail.com>
2024-09-16 11:57:57 +0530

    refactor-2

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import header from "../../assets/Header.png";
 import "./Signup.css";
 import { Link } from "react-router-dom";
 
